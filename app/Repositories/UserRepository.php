48e308409013b0e9d596631b0388ce494314ea51
EyyupkocaYusuf <EyyupkocaYusuf@gmail.com>
2023-10-21 16:51:45 +0300

    UserRepository de register metodu yazildi

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repositories;
 
 use App\Models\User;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Hash;
 
 class UserRepository
 {
@@ -15,6 +17,18 @@ class UserRepository
 
     public function register($data)
     {
+        try {
+            return $this->model->create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]);
+        } catch (QueryException $e){
+            if($e->errorInfo[1] == 1062) {
+                return response()->json(['error'=>'Bu Mail Adresi zaten kullanılmaktadır']);
+            }
+            throw $e;
+        }
 
     }
 
